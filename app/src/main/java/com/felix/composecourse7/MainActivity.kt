e6141ec23606589d7ad450a82d1bedb1c1b3c468
chiumanfelix <chiumanfelix@gmail.com>
2022-10-12 23:43:05 +0700

    Initial commit

---
+++ app/src/main/java/com/felix/composecourse7/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.felix.composecourse7
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.felix.composecourse7.data.DataSource
+import com.felix.composecourse7.model.Topic
+import com.felix.composecourse7.ui.theme.ComposeCourse7Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCourse7Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TopicGrid()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PictureCard(topic: Topic) {
+    Card(
+        elevation = 4.dp
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = topic.imageRes),
+                contentDescription = stringResource(id = topic.name),
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(
+                        top = 16.dp,
+                        start = 16.dp,
+                        end = 16.dp
+                    )
+            ) {
+                Text(
+                    text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Row {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(12.dp)
+                    )
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2)
+    ) {
+        items(DataSource.topics) {
+            PictureCard(topic = it)
+        }
+    }
+}
+/*LazyVerticalGrid(
+    cells = GridCells.Fixed(2),
+    verticalArrangement = Arrangement.spacedBy(8.dp),
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
+    modifier = modifier.padding(8.dp)
+) {
+    items(topicList) { topic ->
+        PictureCard(topic = topic)
+    }
+}*/
+
+
+@Preview(showBackground = true)
+@Composable
+fun PictureCardPrev() {
+    ComposeCourse7Theme {
+        PictureCard(topic = Topic(R.string.architecture, 175, R.drawable.architecture))
+    }
+}
